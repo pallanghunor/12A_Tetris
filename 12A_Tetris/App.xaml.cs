517ec1b87ab16aaa3460113c28f7e573883cd08e
pallanghunor <pallang.hunor@students.jedlik.eu>
2024-01-29 22:31:41 +0100

    Add project files.

---
+++ 12A_Tetris/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace _12A_Tetris
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ 12A_Tetris/GameGrid.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+
+namespace _12A_Tetris
+{
+    public class GameGrid
+    {
+        private int[,] grid;
+        public int Rows { get; private set; }
+        public int Columns { get; private set; }
+
+        public int this[int row, int column]
+        {
+            get { return grid[row, column]; }
+            set { grid[row, column] = value; }
+        }
+
+        public GameGrid(int rows, int columns)
+        {
+            Rows = rows;
+            Columns = columns;
+            grid = new int[rows, columns];
+        }
+
+        public bool IsInside(int row, int column)
+        {
+            return row >= 0 && row < Rows && column >= 0 && column < Columns;
+        }
+
+        public bool IsEmpty(int row, int column)
+        {
+            return IsInside(row, column) && grid[row, column] == 0;
+        }
+
+        public bool IsRowFull(int row)
+        {
+            for (int column = 0; column < Columns; column++)
+            {
+                if (grid[row, column] == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public bool IsRowEmpty(int row)
+        {
+            for (int column = 0; column < Columns; column++)
+            {
+                if (grid[row, column] != 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
